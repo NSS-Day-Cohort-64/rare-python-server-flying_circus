26ae320985ce56126fb044777f7fe884d8fa35f4
Chesney Hardin <chesney.hardin@gmail.com>
2023-07-26 13:55:27 -0500

    initial commit with SQL database tables created

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
-from views.user import create_user, login_user
+from views.users import create_user, login_user
 
 
 class HandleRequests(BaseHTTPRequestHandler):
@@ -60,7 +60,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         content_len = int(self.headers.get('content-length', 0))
         post_body = json.loads(self.rfile.read(content_len))
         response = ''
-        resource, _ = self.parse_url()
+        (resource, _) = self.parse_url()
 
         if resource == 'login':
             response = login_user(post_body)
