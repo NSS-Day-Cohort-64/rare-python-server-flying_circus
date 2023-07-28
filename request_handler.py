a785d7a1e59801037bcce2942ecb18e8d8131aec
Cassie Revell <classierev@gmail.com>
2023-07-28 14:34:47 -0500

    Added Create Subscription and Post Functionality

--- request_handler.py
+++ request_handler.py
@@ -9,7 +9,7 @@ get_all_posts, get_single_post, get_posts_by_category, create_post,
 get_all_post_reactions,
 get_all_comments,
 get_all_categories,
-get_all_post_tags, get_posts_by_user, create_category, delete_post)
+get_all_post_tags, get_posts_by_user, create_category, delete_post, create_subscription)
 
 
 class HandleRequests(BaseHTTPRequestHandler):
@@ -155,7 +155,9 @@ class HandleRequests(BaseHTTPRequestHandler):
         if resource == 'posts':
             response = create_post(post_body)
         if resource == 'tags':
-            response = create_tag(post_body)
+            response = create_tag(post_body) 
+        elif resource == 'subscriptions':
+            response = create_subscription(post_body)
 
         self.wfile.write(response.encode())
 
